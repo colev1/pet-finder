4d58ac9128e7cee9aad46913c682a04d0953d89f
Cole Vanacore <colevanacore@gmail.com>
2019-01-08 16:08:54 -0700

    Is loading shleters test

--- src/reducers/__tests__/sheltersReducer.js
+++ src/reducers/__tests__/sheltersReducer.js
@@ -3,14 +3,22 @@ import * as actions from '../../actions'
 
 describe('shelters  reducer', () => {
   it('should return default state', () => {
-		const expected = ''
-		const result = selectedPet(undefined, {})
+		const expected = []
+		const result = searchedShelters(undefined, {})
 		expect(result).toEqual(expected)
   })
 
   it('should return the state with an array of pets', () => {
-		const petId = 0;
-		const result = selectedPet(undefined, actions.addSelectedPet(petId))
-		expect(result).toEqual(petId)
+    const shelters = [{shelter: 'name'}, {shelter: 'name'} ]
+		const result = searchedShelters(undefined, actions.searchByShelter(shelters))
+		expect(result).toEqual(shelters)
 	})
+})
+
+describe('is loading shelters', () => {
+  it('should return state with a true or false value', () => {
+    const bool = true;
+    const result = isLoadingShelters(false, actions.isLoadingShelters(bool))
+    expect(result).toEqual(bool)
+  })
 })
